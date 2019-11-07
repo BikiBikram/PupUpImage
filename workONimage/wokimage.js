9fd7331fa42a59ca730c51a6603fff359979eae4
BikiBikram <50819100+BikiBikram@users.noreply.github.com>
2019-11-07 10:30:53 +0530

    Add files via upload

---
+++ workONimage/wokimage.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+   
+    $("#fmyBtn").click(function(){
+  
+      $("#myModal1").modal({backdrop: "static"});
+      
+    });
+   
+    $("#smyBtn").click(function(){
+  
+      $("#myModal2").modal({backdrop: "static"});
+      
+    });
+  
+    $("#tmyBtn").click(function(){
+  
+      $("#myModal3").modal({backdrop: "static"});
+      
+    });
+
+    $("#fourmyBtn").click(function(){
+  
+      $("#myModal4").modal({backdrop: "static"});
+      
+    });
+  });
+  
+
+    
+  /*
+
+  js............
+  
+  var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+  function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myBtn").style.display = "none";
+  }
+  */
+
+
+
+
+
+
+
+
+
+
+
+
+
